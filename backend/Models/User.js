d87fa217c0b4d4d243d66ee48591a85690421d91
Wuttiphong2000 <wuttiphong.santhalunai@gmail.com>
2025-06-23 16:02:59 +0700

    debuging

--- backend/Models/User.js
+++ backend/Models/User.js
@@ -1,33 +1,36 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
-  username: { type: String, required: true },
+const userSchema = new mongoose.Schema(
+  {
+    _id: { type: String, required: true },
+    username: { type: String, required: true },
 
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    match: [/\S+@\S+\.\S+/, "Invalid email format"]
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/\S+@\S+\.\S+/, "Invalid email format"],
+    },
+
+    image: {
+      type: String,
+      required: true,
+      match: [/^https?:\/\/.+/, "Image must be a valid URL"],
+    },
+
+    role: {
+      type: String,
+      enum: ["user", "hotelOwner"],
+      default: "user",
+    },
+
+    recentSearchedCities: {
+      type: [String],
+      default: [],
+    },
   },
-
-  image: {
-    type: String,
-    required: true,
-    match: [/^https?:\/\/.+/, "Image must be a valid URL"]
-  },
-
-  role: {
-    type: String,
-    enum: ["user", "hotelOwner"],
-    default: "user"
-  },
-
-  recentSearchedCities: {
-    type: [String],
-    default: []
-  }
-
-}, { timestamps: true });
+  { timestamps: true }
+);
 
 const User = mongoose.model("User", userSchema);
 
