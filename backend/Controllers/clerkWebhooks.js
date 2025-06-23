ff8bfb8c5bf286fecb0e30588846ce21957cd710
Wuttiphong2000 <wuttiphong.santhalunai@gmail.com>
2025-06-23 15:21:59 +0700

    resetcode2

--- backend/Controllers/clerkWebhooks.js
+++ backend/Controllers/clerkWebhooks.js
@@ -20,7 +20,7 @@ const clerkWebhooks = async (req,res) => {
             _id: data.id,
             email: data.email_addresses[0].email_address,
             username: data.first_name + ' ' + data.last_name,
-            image: data.image_url,
+            image: data.image_url
         }
 
         // Switch Cases for different Event
