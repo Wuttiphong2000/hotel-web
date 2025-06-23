fd678e23e2f77c5cf1676125f755bd2c8efd569f
Wuttiphong2000 <wuttiphong.santhalunai@gmail.com>
2025-06-23 14:38:41 +0700

    backend clerkcontroller

---
+++ backend/server.js
@@ -0,0 +1,27 @@
+import express from "express";
+import cors from "cors";
+import connectDB from "./Configs/db.js";
+import "dotenv/config";
+import { clerkMiddleware } from "@clerk/express";
+import clerkWebhooks from "./Controllers/clerkWebhooks.js";
+
+connectDB();
+
+const app = express();
+app.use(cors());
+// Middleware
+app.use(express.json());
+app.use(clerkMiddleware());
+
+// API to listen to Clerk Webhook
+app.use("/api/clerk", clerkWebhooks);
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => res.send("API is Working"));
+
+app.listen(PORT, () => {
+  console.log("Server is running port" + PORT);
+});
+
+export default app;
