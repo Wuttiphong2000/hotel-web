b10af87682c2450bfaf5c9dca0acb6c3ed6a6a80
Wuttiphong Santhalunai <140094791+Wuttiphong2000@users.noreply.github.com>
2025-07-05 21:22:13 +0700

    Update authMiddleware.js

--- backend/Middlewares/authMiddleware.js
+++ backend/Middlewares/authMiddleware.js
@@ -3,7 +3,6 @@ import User from "../Models/User.js";
 export const protect = async (req, res, next) => {
   const auth = req.auth();
   const userId = auth.userId;
-  console.log(userId)
 
   if (!userId) {
     res.json({ success: false, message: "not authenticated" });
